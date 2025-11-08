b7ed3607fa6a5e12a44d5ead9d069802c2ed3bba
Feng Wang <wang6922@outlook.com>
2025-11-08 18:47:13 +0800

    Fix page information

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -18,20 +18,6 @@ export default function AuthLayout({
       <main className="flex-1 flex items-center justify-center">
         {children}
       </main>
-
-      {/* Footer */}
-      <footer className="w-full px-2 py-1 border-t border-border flex text-xs bg-zinc-900 text-zinc-400">
-        <div className="flex-auto">
-          <p>
-            Hello World
-          </p>
-        </div>
-        <div>
-          <p>
-            Powered by <a href="https://github.com/tiny-webui/" className="underline">TinyWebUI</a>.
-          </p>
-        </div>
-      </footer>
     </div>
   );
-} 
+}
